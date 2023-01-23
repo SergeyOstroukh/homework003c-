fcf0c92527aabc80a5a1290cd7b25e006fb4134c
Sergey <hardmass3ffect@gmail.com>
2023-01-23 23:23:18 +0300

    Добавил универсальное решение для первой задачи

---
+++ so Zvezdochkoy/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int Reverse(int number)
+{   
+    int reversedNumber = 0;
+    while (number > 0)
+    {
+        reversedNumber = reversedNumber * 10 + number % 10;
+        number = number / 10;
+
+    }
+    return reversedNumber;
+}
+if (num == Reverse(num)) Console.WriteLine($"Число {num} является палиндроном.");
+else Console.WriteLine($"Число {num} не является палиндромом.");
